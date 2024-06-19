4015669f3dd0c4bb27737ea3e08a9c23183d7206
Eze <eze.gonzalez@vera.com.uy>
2024-06-19 14:59:57 -0300

    Formateo archivo

--- src/main/java/fsGui/Handlers/MvCommand.java
+++ src/main/java/fsGui/Handlers/MvCommand.java
@@ -11,11 +11,12 @@ import fsSim.*;
 public class MvCommand extends BaseCommand {
     public MvCommand(CommandFactory commandFactory) {
         super(commandFactory);
-        super.keywords = new String [] { "mv" };
+        super.keywords = new String[] { "mv" };
     }
 
     @Override
-    protected void internalHandle(String message, ArrayList<String> arguments, StringBuilder response, BigPotatoShell shell) {
+    protected void internalHandle(String message, ArrayList<String> arguments, StringBuilder response,
+            BigPotatoShell shell) {
         String[] mvArgs = message.split(" ");
         if (mvArgs.length != 2) {
             response.append("Uso: mv [origen] [destino]\n");
@@ -25,59 +26,66 @@ public class MvCommand extends BaseCommand {
         String absoluteOrigen = composePath(mvArgs[0], shell.PWD);
         String absoluteDestino = composePath(mvArgs[1], shell.PWD);
         fsIElement origen = shell.fsManager.getElementInFs(absoluteOrigen);
-        fsDir padreDestino = (fsDir) shell.fsManager.getElementInFs(absoluteDestino.substring(0, absoluteDestino.lastIndexOf("/")));
+        fsDir padreDestino = (fsDir) shell.fsManager
+                .getElementInFs(absoluteDestino.substring(0, absoluteDestino.lastIndexOf("/")));
         fsIElement destino = shell.fsManager.getElementInFs(absoluteDestino);
-        if(origen == null) {
+        if (origen == null) {
             response.append("El archivo origen no existe.\n");
             return;
         }
-        // Si el origen es un link, se obtiene la referencia y lo trato como un archivo o directorio normal.
-        if(origen instanceof fsLink) origen = ((fsLink) origen).getReference();
-        if(padreDestino == null) {
+        // Si el origen es un link, se obtiene la referencia y lo trato como un archivo
+        // o directorio normal.
+        if (origen instanceof fsLink)
+            origen = ((fsLink) origen).getReference();
+        if (padreDestino == null) {
             response.append("El directorio destino no existe.\n");
             return;
         }
-        if(padreDestino.getElement(absoluteDestino.substring(absoluteDestino.lastIndexOf("/") + 1)) != null) {
+        if (padreDestino.getElement(absoluteDestino.substring(absoluteDestino.lastIndexOf("/") + 1)) != null) {
             response.append("El archivo destino ya existe.\n");
             return;
         }
-        // TODO para mañana cuando me levante, hay que emprolijar esto, es una basura, puro código redundante.
+        // TODO para mañana cuando me levante, hay que emprolijar esto, es una basura,
+        // puro código redundante.
         // Caso DIR - FILE
-        if(origen instanceof fsDir && destino instanceof fsFile) {
+        if (origen instanceof fsDir && destino instanceof fsFile) {
             response.append("No se puede mover un directorio a un archivo.\n");
             return;
         }
         // Caso FILE - DIR Simple
-        if(origen instanceof fsFile) {
+        if (origen instanceof fsFile) {
             fsFile file = (fsFile) origen;
             padreDestino.move(file);
-            fsDir padreOrigen = (fsDir) shell.fsManager.getElementInFs(absoluteOrigen.substring(0, absoluteOrigen.lastIndexOf("/")));
+            fsDir padreOrigen = (fsDir) shell.fsManager
+                    .getElementInFs(absoluteOrigen.substring(0, absoluteOrigen.lastIndexOf("/")));
             padreOrigen.remove(absoluteOrigen.substring(absoluteOrigen.lastIndexOf("/") + 1));
             response.append("Archivo movido exitosamente.\n");
             return;
         }
         // Caso DIR - DIR Simple
-        if(origen instanceof fsDir) {
+        if (origen instanceof fsDir) {
             fsDir dir = (fsDir) origen;
             padreDestino.move(dir);
-            fsDir padreOrigen = (fsDir) shell.fsManager.getElementInFs(absoluteOrigen.substring(0, absoluteOrigen.lastIndexOf("/")));
+            fsDir padreOrigen = (fsDir) shell.fsManager
+                    .getElementInFs(absoluteOrigen.substring(0, absoluteOrigen.lastIndexOf("/")));
             padreOrigen.remove(absoluteOrigen.substring(absoluteOrigen.lastIndexOf("/") + 1));
             response.append("Directorio movido exitosamente.\n");
             return;
         }
         // Caso FILE - FILE
-        if(origen instanceof fsFile && destino instanceof fsFile) {
-            if((fsFile) origen == (fsFile) destino) {
+        if (origen instanceof fsFile && destino instanceof fsFile) {
+            if ((fsFile) origen == (fsFile) destino) {
                 response.append("El archivo origen y destino son el mismo.\n");
                 return;
             }
             origen.setName(absoluteDestino.substring(absoluteDestino.lastIndexOf("/") + 1));
             padreDestino.move(origen);
-            fsDir padreOrigen = (fsDir) shell.fsManager.getElementInFs(absoluteOrigen.substring(0, absoluteOrigen.lastIndexOf("/")));
+            fsDir padreOrigen = (fsDir) shell.fsManager
+                    .getElementInFs(absoluteOrigen.substring(0, absoluteOrigen.lastIndexOf("/")));
             padreOrigen.remove(absoluteOrigen.substring(absoluteOrigen.lastIndexOf("/") + 1));
             response.append("Archivo movido exitosamente.\n");
             return;
-        } 
+        }
     }
 
     @Override
