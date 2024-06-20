a54cf818af868af92a82bc11e6bc2e07901cfae3
Eze <eze.gonzalez@vera.com.uy>
2024-06-20 00:41:44 -0300

    Spawneamos 3 terminales para refregar nuestra capacidad de mutiusuarios

--- src/main/java/fsGui/Main.java
+++ src/main/java/fsGui/Main.java
@@ -3,5 +3,7 @@ package fsGui;
 public class Main {
     public static void main(String[] args) {
         new SimpleTerminal();
+        new SimpleTerminal();
+        new SimpleTerminal();
     }
 }
