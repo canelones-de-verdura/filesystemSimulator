5f7ca7e701098e5291a1e809a3f4e3ccdfe89cb2
Facundo Méndez Bentos <facumendezbt@gmail.com>
2024-06-21 00:34:26 -0300

    La concurrencia mató al gato

--- src/main/java/fsGui/Main.java
+++ src/main/java/fsGui/Main.java
@@ -1,16 +1,10 @@
 package fsGui;
 
-import javax.swing.SwingUtilities;
-
 public class Main {
     public static void main(String[] args) {
-        Runnable createAndStartTerminal = () -> SwingUtilities.invokeLater(() -> {
-            SimpleTerminal terminal = new SimpleTerminal();
-        });
-
-        // Inicia múltiples instancias de SimpleTerminal
-        createAndStartTerminal.run();
-        createAndStartTerminal.run();
-        createAndStartTerminal.run();
+        for (int i = 0; i < 3; i++) {
+            Thread terminalThread = new Thread(new SimpleTerminal());
+            terminalThread.start();
+        }
     }
 }
